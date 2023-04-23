8b26b0e5e7d6b9d6a21426415c15887af8d0efa5
Rufus <vladimerrybalkin@gmail.com>
2023-04-24 02:23:18 +1000

    1

--- client/src/router.js
+++ client/src/router.js
@@ -4,7 +4,7 @@ import PostsPage from '@/pages/PostsPage.vue';
 import AboutUsPage from '@/pages/AboutUs.vue';
 import ContactUsPage from '@/pages/ContactUs.vue';
 import LocalPostsPage from '@/pages/LocalPostsPage.vue';
-
+ 
 export default createRouter({
     history: createWebHashHistory(),
     routes: [
